f23c286c49d02a8918ac5f5a47da92e5b9f557d3
Timhamer <tfhammersma@gmail.com>
2023-10-17 11:47:22 +0200

    Models + Controllers

---
+++ app/Http/Controllers/CoreTaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CoreTask;
+use Illuminate\Http\Request;
+
+class CoreTaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CoreTask $coreTask)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CoreTask $coreTask)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CoreTask $coreTask)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CoreTask $coreTask)
+    {
+        //
+    }
+}
