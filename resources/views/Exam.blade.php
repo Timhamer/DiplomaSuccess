91fccab2b084b57dc98069932849f8091516e3e1
Jesper117 <jesperminks7@gmail.com>
2023-10-30 16:25:08 +0100

    dingetjes laten zien werkt wel

--- resources/views/Exam.blade.php
+++ resources/views/Exam.blade.php
@@ -1,30 +1,21 @@
 @extends('layouts.app')
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html>
 <head>
     <title>Studenten dashboard</title>
-    {{ $courses }}
+    {{--    <pre>--}}
+    {{--    {{ $courses[0]->coretasks }}--}}
+    {{--    </pre>--}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
-    <div class="card-header kerntaak-header" data-toggle="collapse" data-target="#user-1">
-        <div class="row">
-            <div class="col-sm-11">
-                <label>Kerntaak</label>
-            </div>
-            <div class="col-sm-1">
-                <label>v</label>
-            </div>
-        </div>
-    </div>
-    <div class="collapse" id="user-1">
-        <!-- We need to add additional collapsable layers to show info. -->
-        <div class="card-header werkproces-header" data-toggle="collapse" data-target="#user-1-1">
+    @foreach($courses[0]->coretasks as $coretask)
+        <div class="card-header kerntaak-header" data-toggle="collapse" data-target="#kerntaak-{{$coretask->id}}">
             <div class="row">
                 <div class="col-sm-11">
-                    <label>Werkproces</label>
+                    <label>Kerntaak</label>
                 </div>
                 <div class="col-sm-1">
                     <label>v</label>
@@ -32,37 +23,53 @@
             </div>
         </div>
 
-        <div class="collapse" id="user-1-1">
-            <div class="row">
-                <div class="col-sm-10">
+        <div class="collapse" id="kerntaak-{{$coretask->id}}">
+            @foreach($coretask->workprocesses as $workprocess)
+                <div class="card-header werkproces-header" data-toggle="collapse"
+                     data-target="#workprocess-{{$workprocess->id}}">
                     <div class="row">
-                        <div class="col-sm-6">
-                            <!-- Place your label on the left -->
-                            <label for="task">Task Label</label>
+                        <div class="col-sm-11">
+                            <label>{{$workprocess->name}}</label>
                         </div>
-                        <div class="col-sm-6">
-                            <!-- Place your choice buttons on the right -->
-                            <div class="btn-group" data-toggle="buttons">
-                                <label class="btn">
-                                    <input type="radio" name="options" id="option1"> Button 1
-                                </label>
-                                <label class="btn">
-                                    <input type="radio" name="options" id="option2"> Button 2
-                                </label>
-                                <label class="btn">
-                                    <input type="radio" name="options" id="option2"> Button 3
-                                </label>
-                            </div>
+                        <div class="col-sm-1">
+                            <label>v</label>
                         </div>
                     </div>
                 </div>
-            </div>
 
+                <div class="collapse" id="workprocess-{{$workprocess->id}}">
+                    @foreach($workprocess->tasks as $task)
+                        <div class="row">
+                            <div class="col-sm-10">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <!-- Place your label on the left -->
+                                        <label for="task">{{$task->name}}</label>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <!-- Place your choice buttons on the right -->
+                                        <div class="btn-group" data-toggle="buttons">
+                                            <label class="btn">
+                                                <input type="radio" name="options" id="option1"> Button 1
+                                            </label>
+                                            <label class="btn">
+                                                <input type="radio" name="options" id="option2"> Button 2
+                                            </label>
+                                            <label class="btn">
+                                                <input type="radio" name="options" id="option2"> Button 3
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
+            @endforeach
 
         </div>
-
-    </div>
-
+    @endforeach
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
