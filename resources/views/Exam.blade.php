34a258cb1a24e2aab699c061c1a64525c80d2086
Timhamer <tfhammersma@gmail.com>
2023-11-01 11:09:01 +0100

    Removed debug function

--- resources/views/Exam.blade.php
+++ resources/views/Exam.blade.php
@@ -92,11 +92,6 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
-        $(document).ready(function(){
-            console.log("Hi")
-        })
-    </script>
-    <script>
         $(document).ready(function() {
             $('.task-option').on('click', function() {
                 var selectedValue = $(this).val();
