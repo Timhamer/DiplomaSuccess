9f09c8069c1fcdcca7c82cf7c66b976c0ed29020
Timhamer <tfhammersma@gmail.com>
2023-11-13 15:57:49 +0100

    Bootstrap toegevoegd.

--- resources/views/layouts/edit-exam.blade.php
+++ resources/views/layouts/edit-exam.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
+ <!-- Add these lines to include Bootstrap CSS and JavaScript files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
 
 @section('content')
     <form id="dynamic-form" action="{{ route('saveFormData') }}" method="POST">
